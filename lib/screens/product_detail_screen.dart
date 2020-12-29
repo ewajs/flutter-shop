e103ce641bf4da71c8a93c9a1bc2d6db0beff0af
ewajs <ezequiel.wajs@gmail.com>
2020-12-29 13:58:08 -0300

    Incorporated Provider into detail and product grid

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const String routeName = '/product-detail';
@@ -6,9 +9,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context).settings.arguments as String;
+    // Setting listen to false avoids the widget being recreated when data is updated
+    // since we don't expect the title to change, we can avoid rebuilds by setting this.
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(id);
     return Scaffold(
       appBar: AppBar(
-        title: Text(id),
+        title: Text(loadedProduct.title),
       ),
     );
   }
