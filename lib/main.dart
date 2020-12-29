087e9d7b55d4d7c5995966631991d9e205124bee
ewajs <ezequiel.wajs@gmail.com>
2020-12-29 13:18:21 -0300

    Started working on Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,14 +15,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) =>
+          Products(), // for V3 and below, use builder instead of create.
+      child: MaterialApp(
+        title: 'Flutter Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  final String title;
-
-  ProductDetailScreen(this.title);
+  static const String routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+    final id = ModalRoute.of(context).settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(id),
       ),
     );
   }
