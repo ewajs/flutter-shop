486bf16447cabee07564c23e37ee82b1e84a618b
ewajs <ezequiel.wajs@gmail.com>
2020-12-31 11:16:29 -0300

    Added Cart provider with MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) =>
-          Products(), // for V3 and below, use builder instead of create.
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        )
+      ],
       child: MaterialApp(
         title: 'Flutter Shop',
         theme: ThemeData(
@@ -30,6 +37,6 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
         },
       ),
-    );
+    ); // for V3 and below, use builder instead of create.
   }
 }
